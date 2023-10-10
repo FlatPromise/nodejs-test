19ccbd65e11eea4f7d918c3a9b7669bf332b354e
Edgar <epicerds2@gmail.com>
2023-10-10 13:37:13 +0800

    removed comments in index

--- index.js
+++ index.js
@@ -14,11 +14,6 @@ const connection = mysql.createConnection({
 const port = 3000;
 
 connection.connect();
-// connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-//   if (err) throw err;
-
-//   console.log('The solution is: ', rows[0].solution);
-// });
 // connection.end();
 // connection.resume();
 
